16ddebc5aeb6ddd420db9452e350a17264364921
dmusser14 <dmusser28@gmail.com>
2023-07-16 20:45:35 -0400

    Created new variable pokemonList.
    Created pokemon list with pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-
+let pokemonList = [
+    {name: 'Jigglypuff', height: 1, type: ['fairy', 'normal']},
+    {name: 'Charmander', height: 2, type: ['fire', 'monster']},
+    {name: 'Snorlax', height: 8, type: ['normal', 'monster']},
+    {name: 'Wartortle', height: 4, type: ['water', 'ice']}
+];
