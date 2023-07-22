d9b7bca548295474e2a94bf06ea7d5bb5604ffbc
dmusser14 <dmusser28@gmail.com>
2023-07-22 19:16:03 -0400

    added loop and conditional
    To display Pokemon names and height

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,14 @@ let pokemonList = [
     {name: 'Snorlax', height: 8, type: ['normal', 'monster']},
     {name: 'Wartortle', height: 4, type: ['water', 'ice']}
 ];
+// a loop with a conditional to display all of the pokemon names and height commenting on the tallest one.
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList [i].height >=6){  
+      document.write(pokemonList [i].name + ' (height = ', pokemonList       
+      [i].height + ') - Wow, that\'s big!' + '<br>')
+    }
+    else if (pokemonList [i].height <6){
+      document.write(pokemonList [i].name + ' (height = ', pokemonList       
+      [i].height + ')' + '<br>')
+    }
+  }
