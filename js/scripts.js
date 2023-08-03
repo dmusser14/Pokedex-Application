3d6b29dd64ba0c8821ef64f29c4468b632dc4174
dmusser14 <dmusser28@gmail.com>
2023-08-02 20:49:04 -0400

    adding IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     {name: 'Jigglypuff', height: 1, type: ['fairy', 'normal']},
     {name: 'Charmander', height: 2, type: ['fire', 'monster']},
     {name: 'Snorlax', height: 8, type: ['normal', 'monster']},
     {name: 'Wartortle', height: 4, type: ['water', 'ice']}
 ];
-// a loop with a conditional to display all of the pokemon names and height commenting on the tallest one.
-  pokemonList.forEach(function(pokemon){
+
+  function getAll() {
+    return pokemonList;
+  }
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})()
+// a forEach with a conditional to display all of the pokemon names and height commenting on the tallest one.
+  pokemonRepository.getAll().forEach(function(pokemon){
     if (pokemon.height >=6){  
       document.write(pokemon.name + ' (height = ', pokemon.height + ') - Wow, that\'s big!' + '<br>')
     }
