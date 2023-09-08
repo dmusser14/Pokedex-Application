a1bc5d60bdda2dbd749e91499da96b30a28c0dbc
dmusser14 <dmusser28@gmail.com>
2023-09-07 20:46:51 -0400

    correction to typo in HTML
    Correct typo in HTML that allowed the button to open

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'
-  //let modalContainer = document.querySelector('#modal-container');
+  let modalContainer = document.querySelector('#modal-container');
 
   function getAll() {
     return pokemonList;
